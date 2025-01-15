8f6156f7a01e5c83ced89d3761ce3ba86860b7e5
pranav514 <pranavsalunkhe327@gmail.com>
2025-01-16 00:52:49 +0530

    initiate ice candidate

--- client/src/components/Sender.tsx
+++ client/src/components/Sender.tsx
@@ -4,19 +4,27 @@ import React, { useEffect, useState } from 'react'
 function Sender() {
   const [socket  , setSocket] = useState<WebSocket | null>(null);
   useEffect(() => {
-    const socket = new WebSocket("ws//localhost:8080");
-    setSocket(socket);
+    const socket = new WebSocket("ws://localhost:8080");
     socket.onopen = () => {
       socket.send(JSON.stringify({
         type : 'sender'
       }))
-    }
-  })
+    }    
+setSocket(socket)
+  } , [])
 
   const sendVideo = async () => {
     const pc = new RTCPeerConnection();
     const offer = await pc.createOffer();
-    await pc.setLocalDescription();
+    await pc.setLocalDescription(offer);
+    pc.onicecandidate = (event) => {
+      if(event.candidate){
+        socket?.send(JSON.stringify({
+          type : 'iceCandidate',
+          candidate :  event.candidate
+        }))
+      }
+    }
     socket?.send(JSON.stringify({
       type : 'createOffer',
       sdp : offer
@@ -25,7 +33,11 @@ function Sender() {
       socket.onmessage = async (event) => {
         const message = JSON.parse(event.data);
         if(message.type === 'createAnswer'){
+
           await pc.setRemoteDescription(message.sdp)
+        }
+        if(message.type === 'iceCandidate'){
+           pc.addIceCandidate(message.candidate)
         }
       }
     }
