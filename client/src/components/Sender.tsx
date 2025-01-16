0e516ffc200599dfd16a8af70ffdcc83d7b9f124
pranav514 <pranavsalunkhe327@gmail.com>
2025-01-16 10:16:56 +0530

    added video p2p

--- client/src/components/Sender.tsx
+++ client/src/components/Sender.tsx
@@ -1,53 +1,63 @@
-import React, { useEffect, useState } from 'react'
-
+import React, { useEffect, useState } from "react";
 
 function Sender() {
-  const [socket  , setSocket] = useState<WebSocket | null>(null);
+  const [socket, setSocket] = useState<WebSocket | null>(null);
   useEffect(() => {
     const socket = new WebSocket("ws://localhost:8080");
     socket.onopen = () => {
-      socket.send(JSON.stringify({
-        type : 'sender'
-      }))
-    }    
-setSocket(socket)
-  } , [])
+      socket.send(
+        JSON.stringify({
+          type: "sender",
+        })
+      );
+    };
+    setSocket(socket);
+  }, []);
 
   const sendVideo = async () => {
     const pc = new RTCPeerConnection();
-    const offer = await pc.createOffer();
-    await pc.setLocalDescription(offer);
+    pc.onnegotiationneeded = async () => {
+      console.log("negotiation needed");
+      const offer = await pc.createOffer();
+      await pc.setLocalDescription(offer);
+      socket?.send(
+        JSON.stringify({
+          type: "createOffer",
+          sdp: offer,
+        })
+      );
+    };
+
     pc.onicecandidate = (event) => {
-      if(event.candidate){
-        socket?.send(JSON.stringify({
-          type : 'iceCandidate',
-          candidate :  event.candidate
-        }))
+      if (event.candidate) {
+        socket?.send(
+          JSON.stringify({
+            type: "iceCandidate",
+            candidate: event.candidate,
+          })
+        );
       }
-    }
-    socket?.send(JSON.stringify({
-      type : 'createOffer',
-      sdp : offer
-    }))
-    if(socket){
+    };
+
+    if (socket) {
       socket.onmessage = async (event) => {
         const message = JSON.parse(event.data);
-        if(message.type === 'createAnswer'){
-
-          await pc.setRemoteDescription(message.sdp)
+        if (message.type === "createAnswer") {
+          await pc.setRemoteDescription(message.sdp);
         }
-        if(message.type === 'iceCandidate'){
-           pc.addIceCandidate(message.candidate)
+        if (message.type === "iceCandidate") {
+          pc.addIceCandidate(message.candidate);
         }
-      }
+      };
     }
-
-  }
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+    pc.addTrack(stream.getVideoTracks()[0]);
+  };
   return (
     <div>
-      <button onClick = {sendVideo}>Send the data</button>
+      <button onClick={sendVideo}>Send the data</button>
     </div>
-  )
+  );
 }
 
-export default Sender
+export default Sender;
